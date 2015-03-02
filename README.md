f53769317fd87aa8cae6d6aa86af00fcc8a72374
Javier Cejudo <javier@javiercejudo.com>
2015-03-03 01:56:11 +1100

    docs(README): add usage examples

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Scale normalised data
 ## Install
 
     npm install --save-dev scale-normalised
+
+## Usage
+
+```js
+var scale = require('scale-normalised');
+
+scale(.5, [2, 4]); // returns 3
+scale(-.25, [-3, 5]); // returns -5
+```
--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,9 @@
 var should = require('should');
-var rescale = require('../src/scale.js');
+var scale = require('../src/scale.js');
 
 describe('scale', function() {
   it('should scale normalised data', function() {
-    rescale(.5, [2, 4]).should.be.exactly(3);
-    rescale(-.25, [-3, 5]).should.be.exactly(-5);
+    scale(.5, [2, 4]).should.be.exactly(3);
+    scale(-.25, [-3, 5]).should.be.exactly(-5);
   });
 });
